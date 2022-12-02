838a42d40f92b42bd3bc675f92bee3d6815d625a
Pär Wieslander <wieslander@gmail.com>
2022-12-02 14:10:11 +0100

    Day 2: Refactor logic for determining winners.

--- src/aoc_2022/day02.clj
+++ src/aoc_2022/day02.clj
@@ -4,14 +4,16 @@
 
 (defn parse-line
   [line]
-  (split line #" "))
+  (mapv keyword (split line #" ")))
 
 (def input
   (->> "day02.txt"
        lines
        (map parse-line)))
 
-(def shapes
+(def shapes [:rock :paper :scissors])
+
+(def shapes-from-input
   {:A :rock
    :B :paper
    :C :scissors
@@ -19,49 +21,46 @@
    :Y :paper
    :Z :scissors})
 
-(def shape-scores {:rock 1 :paper 2 :scissors 3})
+(def what-beats
+  {:rock :paper
+   :paper :scissors
+   :scissors :rock})
 
-(def outcomes
-  {[:rock :rock] :draw
-   [:rock :paper] :win
-   [:rock :scissors] :loss
-   [:paper :rock] :loss
-   [:paper :paper] :draw
-   [:paper :scissors] :win
-   [:scissors :rock] :win
-   [:scissors :paper] :loss
-   [:scissors :scissors] :draw
-   :X :loss
-   :Y :draw
-   :Z :win})
+(defn outcome
+  [elf response]
+  (cond
+    (= elf response) :draw
+    (= response (what-beats elf)) :win
+    (= elf (what-beats response)) :loss))
+
+(def shape-scores {:rock 1 :paper 2 :scissors 3})
 
 (def outcome-scores
   {:win 6 :draw 3 :loss 0})
 
-(defn response
-  [opponent-shape outcome]
-  (first (drop-while
-          #(not= (outcomes [opponent-shape %]) outcome)
-          [:rock :paper :scissors])))
+(def desired-outcomes {:X :loss :Y :draw :Z :win})
+
+(defn find-response
+  [elf desired-outcome]
+  (->> shapes
+       (filter #(= (outcome elf %)
+                   desired-outcome))
+       first))
 
 (defn score
   [round]
-  (let [opponent (keyword (get round 0))
-        opponent-shape (opponent shapes)
-        response (keyword (get round 1))
-        response-shape (response shapes)
-        outcome (outcomes [opponent-shape response-shape])]
-    (+ (response-shape shape-scores)
-       (outcome outcome-scores))))
+  (let [elf (shapes-from-input (get round 0))
+        response (shapes-from-input (get round 1))]
+    (+ (shape-scores response)
+       (outcome-scores (outcome elf response)))))
 
 (defn score2
   [round]
-  (let [opponent (keyword (get round 0))
-        opponent-shape (opponent shapes)
-        desired-outcome (keyword (get round 1))
-        outcome (outcomes desired-outcome)]
-    (+ (shape-scores (response opponent-shape outcome))
-       (outcome outcome-scores))))
+  (let [elf (shapes-from-input (get round 0))
+        desired-outcome (desired-outcomes (get round 1))
+        response (find-response elf desired-outcome)]
+    (+ (shape-scores response)
+       (outcome-scores desired-outcome))))
 
 (def part1
   (->> input
