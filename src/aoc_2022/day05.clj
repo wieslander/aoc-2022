9f4afbda9ef0e4238126852111fdf0130a60fed1
Pär Wieslander <wieslander@gmail.com>
2022-12-05 15:22:39 +0100

    Day 5

---
+++ src/aoc_2022/day05.clj
@@ -0,0 +1,113 @@
+(ns aoc-2022.day05
+  (:require [aoc-2022.core :refer [lines parse-int]])
+  (:require [clojure.string :as str]))
+
+(defn get-stack-count
+  [lines]
+  (->> lines
+       (take-while #(not (str/blank? %)))
+       last
+       str/trim
+       (#(last (str/split % #" ")))
+       parse-int))
+
+(defn parse-item
+  [line stack-index]
+  (let [item-pos (inc (* stack-index 4))]
+    (when (> (count line) item-pos)
+      (let [item (nth line item-pos)]
+        (when (and (>= (int item) (int \A)) (<= (int item) (int \Z)))
+          item)))))
+
+(defn parse-items
+  [line stack-count]
+  (->> (range stack-count)
+       (map #(parse-item line %))))
+
+(defn push-item
+  [stack item]
+  (if (nil? item)
+    stack
+    (conj stack item)))
+
+(defn parse-stacks
+  [lines stack-count]
+  (let [initial-stacks (vec (take stack-count (repeatedly list)))
+        stack-lines (take-while #(not (nil? (str/index-of % "["))) lines)]
+    (loop [lines (reverse stack-lines)
+           stacks initial-stacks]
+      (if (empty? lines)
+        stacks
+        (let [line (first lines)
+              items (parse-items line stack-count)]
+          (recur (rest lines) (mapv push-item stacks items)))))))
+
+(defn parse-instruction
+  [line]
+  (let [[_, item-count, _, source, _, dest] (str/split line #" ")]
+    {:item-count (parse-int item-count)
+     :source (parse-int source)
+     :dest (parse-int dest)}))
+
+(defn parse-instructions
+  [lines]
+  (->> lines
+       (drop-while #(not (str/starts-with? % "move")))
+       (map parse-instruction)))
+
+(defn parse-input
+  [lines]
+  (let [stack-count (get-stack-count lines)
+        stacks (parse-stacks lines stack-count)
+        instructions (parse-instructions lines)]
+    {:stacks stacks :instructions instructions}))
+
+(def input
+  (->> "day05.txt"
+       lines
+       parse-input))
+
+(defn move
+  [stacks instruction reverse?]
+  (if (zero? (:item-count instruction))
+    stacks
+    (let [{source-index :source
+           dest-index :dest
+           item-count :item-count} instruction
+          source-stack (get stacks (dec source-index))
+          dest-stack (get stacks (dec dest-index))
+          items (take item-count source-stack)
+          ordered-items (if reverse? (reverse items) items)
+          remaining (drop item-count source-stack)]
+      (assoc stacks
+             (dec source-index) remaining
+             (dec dest-index) (concat ordered-items dest-stack)))))
+
+(defn move-items
+  [stacks [instruction & remaining-instructions]]
+  (if (nil? instruction)
+    stacks
+    (recur (move stacks instruction true) remaining-instructions)))
+
+(defn move-items-ordered
+  [stacks [instruction & remaining-instructions]]
+  (if (nil? instruction)
+    stacks
+    (recur (move stacks instruction false) remaining-instructions)))
+
+(def part1
+  (->> (move-items (:stacks input) (:instructions input))
+       (map first)
+       (filter #(not (nil? %)))
+       (apply str)))
+
+(def part2
+  (->> (move-items-ordered (:stacks input) (:instructions input))
+       (map first)
+       (filter #(not (nil? %)))
+       (apply str)))
+
+(defn -main
+  []
+  (println part1)
+  (println part2))
